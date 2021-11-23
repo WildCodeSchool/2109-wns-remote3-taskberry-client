ba79862bc4a33bd2670345540f57f1d217f551d2
Quentin <dev.mahjopi@mahjopi.com>
2021-11-23 12:10:04 +0100

    init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ],
+    "no-console": "off",
+    "no-alert": "off",
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+  },
+  ignorePatterns: [".eslintrc.js"],
+};
