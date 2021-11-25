ae9c806528c96c775ebaf6376e8445db11c272d0
Myriamhi <m.helaoui@it-students.fr>
2021-11-25 16:29:42 +0100

    intégration colonnes board projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import ProjectBoard from "./components/ProjectBoard";
 function App() {
   return (
-    <div className="App">
+    <div className="App flex">
       <Navbar />
+      <ProjectBoard />
     </div>
   );
 }
---
+++ src/components/ProjectBoard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ProjectBoard() {
+  return (
+    <div className="w-screen  px-10 bg-white md:flex  items-end mb-16 ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+        <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
+          À faire
+        </div>
+      </div>
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+        <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
+          En cours
+        </div>
+      </div>
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+        <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
+          Review
+        </div>
+      </div>
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+        <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
+          Terminé
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectBoard;
