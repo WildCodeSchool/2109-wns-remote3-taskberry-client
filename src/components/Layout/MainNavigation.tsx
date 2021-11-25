1bd2beb8c9493bf03ecfbc1a2814d6256dcf373d
Guy de Messimy <gdemessimy@gmail.com>
2021-11-25 15:00:31 +0100

    WIP: css

--- src/components/Layout/MainNavigation.tsx
+++ src/components/Layout/MainNavigation.tsx
@@ -4,8 +4,16 @@ const MainNavigation : React.FC = () => {
   return (
     <header data-test="main-navigation-content">
       <Link to="/">
+<<<<<<< HEAD
         <div>TaskBerry Auth</div>
         <img src="/images/TaskBerry.png" alt="logo" />
+=======
+        <img
+          className="w-[110px] h-[80px] border-solid border-1 border-red-500 m-0 "
+          src="/images/Berry.png"
+          alt="logo"
+        />
+>>>>>>> WIP: css
       </Link>
       <nav>
         <ul>
@@ -25,3 +33,10 @@ const MainNavigation : React.FC = () => {
 };
 
 export default MainNavigation;
+<<<<<<< HEAD
+=======
+
+{
+  /* <button className="text-white-200 border-solid border-1 border-white-800 font-bold px-2 py-6 rounded-sm cursor-pointer"> */
+}
+>>>>>>> WIP: css
--- src/components/StartingPage/StartingPageContent.tsx
+++ src/components/StartingPage/StartingPageContent.tsx
@@ -1,8 +1,26 @@
 const StartingPageContent: React.FC = () => {
   return (
+<<<<<<< HEAD
     <section data-test="starting-page-content">
       <h1>Welcome on TaskBerry!</h1>
     </section>
+=======
+    <div>
+      <section
+        className="m-24 m-auto text-center"
+        data-test="starting-page-content"
+      >
+        <h1 className="text-6xl text-purple-900">Welcome on TaskBerry!</h1>
+      </section>
+      <div className="w-full flex justify-center">
+        <img
+          className="w-[462px] h-[418px] border-solid border-1 border-purple-500 "
+          src="/images/TaskBerry.png"
+          alt="logo"
+        />
+      </div>
+    </div>
+>>>>>>> WIP: css
   );
 };
 
