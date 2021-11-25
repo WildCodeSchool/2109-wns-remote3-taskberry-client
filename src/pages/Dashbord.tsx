e2c9776b29f75582c499ca619f9cd3ef5e847576
Quentin <dev.mahjopi@mahjopi.com>
2021-11-25 15:25:20 +0100

    add card project

--- src/pages/Dashbord.tsx
+++ src/pages/Dashbord.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Cardproject from "./Cardproject";
 
 function Dashbord() {
   return (
@@ -8,7 +9,11 @@ function Dashbord() {
           <div className="rounded-full w-[125px] py-2 px-6 bg-purple-dark text-white absolute right-0 font-bold">
             À faire
           </div>
-          <div className="flex flex-col bg-gray-ligth w-[350px] h-[700px] rounded-2xl shadow-lg mt-5"></div>
+          <div className="flex flex-col bg-gray-ligth w-[350px] h-[700px] rounded-2xl shadow-lg mt-5 justify-start items-center">
+            <div className="mt-9">
+              <Cardproject />
+            </div>
+          </div>
         </div>
         <div className="relative w-[370px] h-[720px] flex align-end">
           <div className="rounded-full w-[125px] py-2 px-6 bg-purple-dark text-white absolute right-0 font-bold">
