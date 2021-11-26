8c66e63e38bff1b50a69608816f7b62bdfae62df
Myriamhi <m.helaoui@it-students.fr>
2021-11-26 11:03:46 +0100

    add composant Cardticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import ProjectBoard from "./components/ProjectBoard";
+
 function App() {
   return (
     <div className="App flex">
       <Navbar />
-      <ProjectBoard />
     </div>
   );
 }
