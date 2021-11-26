7197c8864ac43cdf066f2295d796372fe5ab1970
Quentin <dev.mahjopi@mahjopi.com>
2021-11-26 11:10:23 +0100

    add card new project and profil user with avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Dashbord from "./pages/Dashbord";
 
 function App() {
   return (
     <div className="App flex">
       <Navbar />
+      <Dashbord />
     </div>
   );
 }
---
+++ src/components/CardAddProject.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+function CardAddProject() {
+  return (
+    <div className="bg-white rounded-2xl h-[170px] w-[290px] flex">
+      <div className="p-5 w-full space-y-6">
+        <div className="flex-col">
+          <p className="text-xl text-center">Nouveau projet</p>
+        </div>
+        <div className="flex-col">
+          <FontAwesomeIcon icon={faPlus} size="5x" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardAddProject;
