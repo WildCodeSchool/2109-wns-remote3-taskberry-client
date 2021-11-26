fc4a73f6cd44e24dc810047550557b484c232b1f
Quentin <dev.mahjopi@mahjopi.com>
2021-11-26 11:11:12 +0100

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Dashbord from "./pages/Dashbord";
 
 function App() {
   return (
     <div className="App flex">
       <Navbar />
-      <Dashbord />
     </div>
   );
 }
