7c121ef0bfe764e127cad1068a282aff8578d50e
Myriamhi <m.helaoui@it-students.fr>
2021-11-26 11:03:46 +0100

    add composant Cardticket

--- src/pages/ProjectBoard.tsx
+++ src/pages/ProjectBoard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Cardticket from "./Cardticket";
 
 function ProjectBoard() {
   return (
     <div className="w-screen  px-10 bg-white md:flex  items-end mb-16 ">
-      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-4 p-5">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
           À faire
         </div>
+        <Cardticket />
       </div>
       <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
