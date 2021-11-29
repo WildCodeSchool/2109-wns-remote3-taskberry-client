2d2bfc8b894a3db97ce1d6bab8bc7ff0fe0f2713
Guy de Messimy <gdemessimy@gmail.com>
2021-11-29 11:25:10 +0100

    move toggle button

--- src/components/Button/AuthButton.tsx
+++ src/components/Button/AuthButton.tsx
@@ -4,7 +4,11 @@ interface AuthButtonProps {
 
 const AuthButton = (props: AuthButtonProps) => {
   const { logged } = props;
-  return <button>{logged ? "Se connecter" : "S'inscrire"}</button>;
+  return (
+    <button className="cursor-pointer text-white bg-purple-400 border-purple-400 border-solid rounded-md py-2 px-10 hover:bg-pink-400 hover:border-pink-400">
+      {logged ? "Se connecter" : "S'inscrire"}
+    </button>
+  );
 };
 
 export default AuthButton;
