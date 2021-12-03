c2e8f38c16a81675588cbeea4c68610505c7e42f
Guy de Messimy <gdemessimy@gmail.com>
2021-12-04 00:06:19 +0100

    fix css and ts

--- src/components/Button/AuthToggleButton.tsx
+++ src/components/Button/AuthToggleButton.tsx
@@ -1,11 +1,16 @@
-const AuthToggleButton = () => {
+interface AuthToggleButtonProps {
+  logged?: boolean;
+  switchAuthModeHandler: () => void;
+}
+const AuthToggleButton = (props: AuthToggleButtonProps) => {
+  const { logged, switchAuthModeHandler } = props;
   return (
     <button
       type="button"
       className="mt-4 bg-transparent text-purple-600 border-none py-0.5 px-6 hover:bg-transparent hover:text-pink-600"
       onClick={switchAuthModeHandler}
     >
-      {isLogin ? "S'inscrire" : "Déjà un compte ? Connectez-vous"}
+      {logged ? "S'inscrire" : "Déjà un compte ? Connectez-vous"}
     </button>
   );
 };
