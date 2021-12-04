a4e22f252221e0cee602c35b95bc8109aa437885
Guy de Messimy <gdemessimy@gmail.com>
2021-12-05 00:57:19 +0100

    add sumit onsign and onlog

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -1,12 +1,64 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import AuthButton from "../Button/AuthButton";
 import AuthToggleButton from "../Button/AuthToggleButton";
 
 const AuthForm = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
   const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const switchAuthModeHandler = (): void => {
     setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current!.value;
+    const enteredPassword = passwordInputRef.current!.value;
+    setIsLoading(true);
+    let url;
+    // need to create hook / async await / validate
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCU6TjWTOafIRK2LwxNhVJ91WZYUX1PyRc";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCU6TjWTOafIRK2LwxNhVJ91WZYUX1PyRc";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            // show an error modal
+            const errorMessage = "Authentification failed";
+            // if (data && data.error & data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   const createAccountFields = !isLogin && (
@@ -19,7 +71,7 @@ const AuthForm = () => {
           className="bg-gray-50 text-purple rounded-md border-white border-solid w-full text-left p-1"
           type="text"
           id="name"
-          required
+          // required
         />
       </div>
       <div className="mb-2">
@@ -30,7 +82,7 @@ const AuthForm = () => {
           className="bg-gray-50 text-purple rounded-md border-white border-solid w-full text-left p-1"
           type="text"
           id="surname"
-          required
+          // required
         />
       </div>
     </div>
@@ -45,7 +97,7 @@ const AuthForm = () => {
         className="bg-gray-50 text-purple rounded-md border-white border-solid w-full text-left p-1"
         type="password"
         id="confirmPassword"
-        required
+        // required
       />
     </div>
   );
@@ -55,7 +107,7 @@ const AuthForm = () => {
       <h1 className="text-center text-white p-4 ">
         {isLogin ? "Se connecter" : "S'inscrire"}
       </h1>
-      <form>
+      <form onSubmit={submitHandler}>
         {createAccountFields}
         <div className="mb-2">
           <label className="block text-white font-bold mb-2" htmlFor="email">
@@ -66,6 +118,7 @@ const AuthForm = () => {
             type="email"
             id="email"
             required
+            ref={emailInputRef}
           />
         </div>
         <div className="mb-2">
@@ -77,11 +130,12 @@ const AuthForm = () => {
             type="password"
             id="password"
             required
+            ref={passwordInputRef}
           />
         </div>
         {confirmPasswordField}
         <div className="mt-6 flex flex-col items-center">
-          <AuthButton logged={isLogin} />
+          <AuthButton logged={isLogin} load={isLoading} />
           <AuthToggleButton
             switchAuthModeHandler={switchAuthModeHandler}
             logged={isLogin}
--- src/components/Button/AuthButton.tsx
+++ src/components/Button/AuthButton.tsx
@@ -1,13 +1,19 @@
 interface AuthButtonProps {
   logged?: boolean;
+  load?: boolean;
 }
 
 const AuthButton = (props: AuthButtonProps) => {
-  const { logged } = props;
+  const { logged, load } = props;
   return (
-    <button className="cursor-pointer text-white bg-purple-400 border-purple-400 border-solid rounded-md py-2 px-10 hover:bg-pink-400 hover:border-pink-400">
-      {logged ? "Se connecter" : "S'inscrire"}
-    </button>
+    <div>
+      {!load && (
+        <button className="cursor-pointer text-white bg-purple-400 border-purple-400 border-solid rounded-md py-2 px-10 hover:bg-pink-400 hover:border-pink-400">
+          {logged ? "Se connecter" : "S'inscrire"}
+        </button>
+      )}
+      {load && <p>Sending request ...</p>}
+    </div>
   );
 };
 
