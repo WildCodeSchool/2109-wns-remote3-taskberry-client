c640de726b6a5503f61873c843ad5835e4e2f895
Guy de Messimy <gdemessimy@gmail.com>
2021-12-20 00:43:57 +0100

    update : fix condtionnal submit and error submit

--- src/components/Button/AuthButton.tsx
+++ src/components/Button/AuthButton.tsx
@@ -1,19 +1,19 @@
 interface AuthButtonProps {
-  logged?: boolean;
+  isLogin?: boolean;
   isLoading?: boolean;
 }
 
 const AuthButton = (props: AuthButtonProps) => {
-  const { logged, isLoading } = props;
+  const { isLogin, isLoading } = props;
   return (
     <div>
       {!isLoading && (
         <button
           className="cursor-pointer text-white bg-purple-400 border-purple-400 border-solid rounded-md py-2 px-10 hover:bg-pink-400 hover:border-pink-400"
           // add conditionnal : if error => disable if not enable / add css
-          // disabled={true}
+          // disabled={errors ? true : false}
         >
-          {logged ? "Se connecter" : "S'inscrire"}
+          {isLogin ? "Se connecter" : "S'inscrire"}
         </button>
       )}
       {isLoading && <p>Sending request ...</p>}
--- src/components/Button/AuthToggleButton.tsx
+++ src/components/Button/AuthToggleButton.tsx
@@ -1,16 +1,16 @@
 interface AuthToggleButtonProps {
-  logged?: boolean;
+  isLogin?: boolean;
   switchAuthModeHandler: () => void;
 }
 const AuthToggleButton = (props: AuthToggleButtonProps) => {
-  const { logged, switchAuthModeHandler } = props;
+  const { isLogin, switchAuthModeHandler } = props;
   return (
     <button
       type="button"
       className="mt-4 bg-transparent text-purple-600 border-none py-0.5 px-6 hover:bg-transparent hover:text-pink-600"
       onClick={switchAuthModeHandler}
     >
-      {logged ? "S'inscrire" : "Déjà un compte ? Connectez-vous"}
+      {isLogin ? "S'inscrire" : "Déjà un compte ? Connectez-vous"}
     </button>
   );
 };
