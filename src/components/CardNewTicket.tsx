b439fa8e98c67f708335afac08ed1f7c8e01d8ef
Myriamhi <m.helaoui@it-students.fr>
2022-01-20 17:46:46 +0100

    créé pop up

--- src/components/CardNewTicket.tsx
+++ src/components/CardNewTicket.tsx
@@ -2,19 +2,42 @@ import React, { useState } from "react";
 
 function CardNewTicket() {
   return (
-    <div className="w-full p-2 my-4 bg-white rounded-2xl flex-col flex-wrap">
-      <div className="flex-col border-b border-gray-500 py-2 w-[150px]">
+    <div className="flex flex-col space-y-4 animated fadeIn faster  fixed  left-0 top-0 flex justify-center bg-gray-900/80 items-center inset-0 z-50 outline-none focus:outline-none ">
+      <div className="w-[800px] h-[400px] flex flex-col p-8 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl">
         <input
-          className="text-xl appearance-none bg-transparent border-none w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none focus:ring-transparent"
-          type="text"
+          className="text-xl rounded-xl p-4 mb-2"
+          type="textarea"
           placeholder="Titre du ticket"
         ></input>
-      </div>
-
-      <div className="flex-col text-center">
-        <button className="bg-indigo-900  my-3 hover:bg-purple-dark text-white font-bold py-1 px-4 rounded-3xl ">
-          Créer
-        </button>
+        <input
+          className="text-xl rounded-xl p-4 mb-2 "
+          type="textarea"
+          placeholder="Description"
+        ></input>
+        <div className="flex">
+          <label> Statut :</label>
+          <select name="status" id="select-status">
+            <option value="">En cours</option>
+            <option value="">Code review</option>
+            <option value="">Terminé</option>
+          </select>
+        </div>
+        <div className="flex">
+          <label> Membre assigné :</label>
+          <select name="members" id="members-assigned">
+            <option value="">John</option>
+            <option value="">Jane</option>
+            <option value="">Anne</option>
+          </select>
+        </div>
+        <div className="flex-col text-center">
+          <button
+            type="submit"
+            className="bg-indigo-900  my-3 hover:bg-purple-dark text-white font-bold py-1 px-4 rounded-3xl "
+          >
+            Créer
+          </button>
+        </div>
       </div>
     </div>
   );
