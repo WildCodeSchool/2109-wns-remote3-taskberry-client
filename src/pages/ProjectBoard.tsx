8306406b541dea5696baab718708f8806bea8907
Myriamhi <m.helaoui@it-students.fr>
2022-01-21 09:17:47 +0100

    modifie input formulaire

--- src/pages/ProjectBoard.tsx
+++ src/pages/ProjectBoard.tsx
@@ -7,33 +7,31 @@ import { useState } from "react";
 const ProjectBoard = () => {
   const [newTicket, setNewTicket] = useState<boolean>(false);
   return (
-    <div className="w-screen  px-10 bg-white md:flex  items-end mb-16 ">
-      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-4 p-5">
+    <div className="min-h-screen ml-10 px-4 bg-white md:flex  items-end ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-[800px] flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-4 p-5">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
           À faire
         </div>
         <Cardticket />
         {!newTicket ? (
-          <a onClick={() => setNewTicket(true)}>
+          <div className="w-full self-end" onClick={() => setNewTicket(true)}>
             <CardAddTicket />
-          </a>
-        ) : (
-          <div>
-            <CardNewTicket />
           </div>
+        ) : (
+          <CardNewTicket />
         )}
       </div>
-      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-[800px] flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
           En cours
         </div>
       </div>
-      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-[800px] flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
           Review
         </div>
       </div>
-      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-4/5 flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
+      <div className=" relative bg-gray-100 m-8 md:w-1/4 h-[800px] flex  flex-wrap  shadow-lg sm:rounded-3xl sm:p-20 ">
         <div className="absolute -right-3 -top-3 bg-purple-dark rounded-full flex justify-center items-center py-2 px-6 text-white font-bold sm:text-xl">
           Terminé
         </div>
