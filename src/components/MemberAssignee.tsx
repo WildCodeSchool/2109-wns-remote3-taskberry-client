719238e44da47e901df930126c19d31d235e736c
Myriamhi <m.helaoui@it-students.fr>
2022-04-01 15:50:25 +0200

    fix PR comments

--- src/components/MemberAssignee.tsx
+++ src/components/MemberAssignee.tsx
@@ -1,12 +1,11 @@
-import { useQuery } from "@apollo/client";
-import React, { useEffect, useState } from "react";
-import { GET_PROJECT_MEMBERS } from "../GraphQL/API";
-
 interface IMemberAssignee {
-  assignee: any;
+  assigneePicture: string;
 }
-export default function MemberAssignee({ assignee }: IMemberAssignee) {
+export default function MemberAssignee({ assigneePicture }: IMemberAssignee) {
   return (
-    <img src={assignee} className="h-[60px] w-[60px] self-end rounded-full" />
+    <img
+      src={assigneePicture}
+      className="h-[60px] w-[60px] self-end rounded-full"
+    />
   );
 }
